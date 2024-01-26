4e18ed279a72fe9b2f3ff77e32108be8d605c7d5
hassanshakil22 <hassanshakil.workspace@gmail.com>
2024-01-26 22:57:55 +0500

    adding 6 answers

--- assignments/assignment_3/ans_5.dart
+++ assignments/assignment_3/ans_5.dart
@@ -1,1 +1,18 @@
-
+// Q.5: Create a map with name, phone keys and store some values to it. Use
+// where to find all keys that have length 4.
+// =
+void main() {
+  Map<String, dynamic> info = {
+    "name": ["hassan", "ali", "usman", "huzaifa", "Rehman", "Raza"],
+    "Phone": [
+      012313213123,
+      012313213563,
+      012313213423,
+      012313213133,
+      012313213443
+    ],
+    "dept": ["civil", "bscs"]
+  };
+  print(info.keys.where((key) => key.length == 4));
+  //only name and dept have are the key having length = 4
+}
