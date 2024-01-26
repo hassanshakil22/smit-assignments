e0b3992e6d65ce895763a450d524cab310400436
hassanshakil22 <hassanshakil.workspace@gmail.com>
2024-01-26 23:36:45 +0500

    adding ans 8

--- assignments/assignment_3/ans_8.dart
+++ assignments/assignment_3/ans_8.dart
@@ -1,1 +1,16 @@
+// Q.8: remove all false values from below list by using removeWhere or
+// retainWhere property.
+void main() {
+  List<Map> usersEligibility = [
+    {'name': 'John', 'eligible': true},
+    {'name': 'Alice', 'eligible': false},
+    {'name': 'Mike', 'eligible': true},
+    {'name': 'Sarah', 'eligible': true},
+    {'name': 'Tom', 'eligible': false},
+  ];
+  // for (var i = 0; i < usersEligibility.length; i++) {
 
+  usersEligibility.removeWhere((element) => element['eligible'] == false);
+  print(usersEligibility);
+  // }
+}
