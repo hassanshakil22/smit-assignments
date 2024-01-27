b7393d61ee3e0b0c1caf918e1b92b88024d699e5
hassanshakil22 <hassanshakil.workspace@gmail.com>
2024-01-27 15:42:45 +0500

    adding questions 22

--- assignments/assignment_3/ans_21.dart
+++ assignments/assignment_3/ans_21.dart
@@ -1,3 +1,8 @@
+// Q.21: Given a map representing a user with keys "name", "isAdmin", and
+// "isActive", write Dart code to check if the user is an active admin. If the user is
+// both an admin and active, print "Active admin", otherwise print "Not an active
+// admin".
+
 void main() {
   Map user = {"name": "hassan", "isAdmin": false, "isActive": true};
 
--- assignments/assignment_3/ans_22.dart
+++ assignments/assignment_3/ans_22.dart
@@ -1,1 +1,16 @@
-
+void main() {
+  Map shoppingCart = {
+    "Apple": 2,
+    "Banana": 4,
+    "Mango": 6,
+    "Cream": 2,
+    "Blueberry": 8,
+    "PineApple": 21,
+    "Orange": 26
+  };
+  if (shoppingCart.containsKey("Apple")) {
+    print("Product found in cart with quantity ${shoppingCart["Apple"]}");
+  } else {
+    print("Product not found in cart");
+  }
+}
