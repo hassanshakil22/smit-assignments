28d7b60dc0433f7e649ffacad0c4bbc53db35dff
hassanshakil22 <hassanshakil.workspace@gmail.com>
2024-01-27 15:33:36 +0500

    adding question 21

--- assignments/assignment_3/ans_20.dart
+++ assignments/assignment_3/ans_20.dart
@@ -1,7 +1,12 @@
+// Q.20: Create a map named "car" with the following key-value pairs: "brand" as
+// "Toyota", "color" as "Red", "isSedan" as true. Write Dart code to check if the
+// car is a sedan and red in color. Print "Match" if both conditions are true,
+// otherwise print "No match"
+
 void main() {
   Map car = {
     "brand": "Toyota",
-    "Color": "Reddd",
+    "Color": "Red",
     "IsSedan": true,
   };
   if (car["Color"] == "Red" && car["IsSedan"] == true) {
--- assignments/assignment_3/ans_21.dart
+++ assignments/assignment_3/ans_21.dart
@@ -1,1 +1,17 @@
+void main() {
+  Map user = {"name": "hassan", "isAdmin": false, "isActive": true};
 
+  if (user["isAdmin"] == true) {
+    if (user["isActive"] == true) {
+      print("Admin is Active");
+    } else {
+      print("user is Admin but not active");
+    }
+  } else {
+    if (user["isActive"] == true) {
+      print("not an active admin");
+    } else {
+      print("no user is active");
+    }
+  }
+}
