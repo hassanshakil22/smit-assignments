15c79fb296bb3d2308d2b1d74281bd3483d13221
hassanshakil22 <hassanshakil.workspace@gmail.com>
2024-01-27 15:10:14 +0500

    adding question 18

--- assignments/assignment_3/ans_18.dart
+++ assignments/assignment_3/ans_18.dart
@@ -1,1 +1,13 @@
+void main() {
+  Map person = {
+    "name": "john",
+    "age": 25,
+    "isStudent": false,
+  };
 
+  if (person["age"] > 18 && person["isStudent"] == true) {
+    print("eligible");
+  } else {
+    print("Not eligible");
+  }
+}
