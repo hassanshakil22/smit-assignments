6ef9ac2caef27404204ef8f9949a83455d63f677
hassanshakil22 <hassanshakil.workspace@gmail.com>
2024-01-27 13:52:06 +0500

    adding question 15

--- assignments/assignment_3/ans_15.dart
+++ assignments/assignment_3/ans_15.dart
@@ -1,1 +1,8 @@
+void main() {
+  List<int> integers = [2, 4, 5, 7, -8, 9, -7, 6, 5, 4, 3, -3, -2];
 
+  var newList = integers.where((element) => element > 0);
+  newList.toList();
+  print("orignal list $integers");
+  print("positive list $newList");
+}
