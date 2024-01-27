a3786b557f6108ede5b4a8d800aadf8522cc1f16
hassanshakil22 <hassanshakil.workspace@gmail.com>
2024-01-27 15:19:28 +0500

    adding question 19'

--- assignments/assignment_3/ans_19.dart
+++ assignments/assignment_3/ans_19.dart
@@ -1,1 +1,9 @@
+void main() {
+  Map product = {"name": "IPhone", "Price": 150000, "Quantity": -2};
 
+  if (product["Quantity"] > 0) {
+    print("${product["name"]} is in Stock");
+  } else {
+    print("${product["name"]} is in Out of Stock");
+  }
+}
