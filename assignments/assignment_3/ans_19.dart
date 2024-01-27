10766ff22de60fc5f50b113f41ca8f2e78d5e8d8
hassanshakil22 <hassanshakil.workspace@gmail.com>
2024-01-27 15:24:27 +0500

    adding question 20

--- assignments/assignment_3/ans_19.dart
+++ assignments/assignment_3/ans_19.dart
@@ -1,5 +1,9 @@
+// Q.19: Given a map representing a product with keys "name", "price", and
+// "quantity", write Dart code to check if the product is in stock. If the quantity is
+// greater than 0, print "In stock", otherwise print "Out of stock".
+
 void main() {
-  Map product = {"name": "IPhone", "Price": 150000, "Quantity": -2};
+  Map product = {"name": "IPhone", "Price": 150000, "Quantity": 2};
 
   if (product["Quantity"] > 0) {
     print("${product["name"]} is in Stock");
--- assignments/assignment_3/ans_20.dart
+++ assignments/assignment_3/ans_20.dart
@@ -1,1 +1,12 @@
-
+void main() {
+  Map car = {
+    "brand": "Toyota",
+    "Color": "Reddd",
+    "IsSedan": true,
+  };
+  if (car["Color"] == "Red" && car["IsSedan"] == true) {
+    print("match");
+  } else {
+    print("No match");
+  }
+}
