6282d1a7cc3abf9f76c21d8cc835e45f31dad459
hassanshakil22 <hassanshakil.workspace@gmail.com>
2024-01-27 11:52:45 +0500

    adding question 1o

--- assignments/assignment_3/ans_10.dart
+++ assignments/assignment_3/ans_10.dart
@@ -1,1 +1,16 @@
+// Q.10: Write a Dart code that takes in a list of strings and removes any duplicate
+// elements, returning a new list without duplicates. The order of elements in the
+// new list should be the same as in the original list.
 
+void main() {
+  List<String> LiOfStr = ["abc", "xyz", "pqr", "mno", "pqr", "xyz", "abc"];
+  List<String> newLi = [];
+  for (var i = 0; i < LiOfStr.length; i++) {
+    if (newLi.contains(LiOfStr[i])) {
+    } else {
+      newLi.add(LiOfStr[i]);
+    }
+  }
+  print(LiOfStr);
+  print(newLi);
+}
