3a4a55453fe917d869054ac0cfc0c04539aa77b8
hassanshakil22 <hassanshakil.workspace@gmail.com>
2024-01-27 12:15:55 +0500

    adding question 12

--- assignments/assignment_3/ans_11.dart
+++ assignments/assignment_3/ans_11.dart
@@ -1,3 +1,6 @@
+// Q 11: Write a Dart code that takes in a list and an integer n as parameters. The
+// program should print a new list containing the first n elements from the original
+// list.
 void main() {
   List<int> integers = [2, 4, 5, 7, 8, 9, 7, 6, 5, 4, 3, 3, 2];
   int toPrint = 5;
--- assignments/assignment_3/ans_12.dart
+++ assignments/assignment_3/ans_12.dart
@@ -1,1 +1,7 @@
-
+void main() {
+  List anylist = ["nothing", "anything", "something", "thing"];
+  List newlist = [];
+  newlist.addAll(anylist.reversed);
+  print(anylist);
+  print(newlist);
+}
