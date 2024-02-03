57111f5467f62449660dcf013a26876649f30fd0
hassanshakil22 <hassanshakil.workspace@gmail.com>
2024-02-03 18:57:29 +0500

    adding ans 9

--- assignments/assignment_4/Ans9.dart
+++ assignments/assignment_4/Ans9.dart
@@ -1,1 +1,10 @@
-question9
+// question9: Write a program to display the cube of the number up to an integer.
+
+import 'dart:math';
+
+void main() {
+  int n = 6;
+  for (var i = 0; i < n; i++) {
+    print("Number is : $i and cube of  $i is : ${pow(i, 3)}");
+  }
+}
