fb95f26012f8f4c854afb70d652dcc5a484f838c
hassanshakil22 <hassanshakil.workspace@gmail.com>
2024-02-03 16:34:51 +0500

    adding question 3

--- assignments/assignment_4/Ans3.dart
+++ assignments/assignment_4/Ans3.dart
@@ -1,1 +1,17 @@
-question3
+// question3:Implement a code that checks whether a given number is prime or not.
+
+void main() {
+  int number = 12313;
+  int divisiblity = 0;
+  for (var i = 2; i < number; i++) {
+    if (number % i == 0) {
+      divisiblity++;
+    }
+  }
+  if (divisiblity > 0) {
+    print("$number is not a prime number");
+  } else {
+    print("$number is a prime number");
+  }
+  print(divisiblity);
+}
