a86902e520c5641f2c84fef7db3b0008e348d935
hassanshakil22 <hassanshakil.workspace@gmail.com>
2024-02-03 18:15:25 +0500

    adding question 6

--- assignments/assignment_4/Ans6.dart
+++ assignments/assignment_4/Ans6.dart
@@ -1,1 +1,20 @@
-// question6
+// question6 Implement a code that finds the largest element in a list using a for
+// loop.[3, 9, 1, 6, 4, 2, 8, 5, 7]
+
+void main() {
+  List numbers = [3, 9, 1, 6, 4, 2, 8, 5, 7];
+  int smallestNumber = numbers[0];
+  int largestNumber = numbers[0];
+
+  for (var i in numbers) {
+    if (i > largestNumber) {
+      largestNumber = i;
+    }
+    if (i < smallestNumber) {
+      smallestNumber = i;
+    }
+  }
+
+  print("largest number $largestNumber");
+  print("smallest number $smallestNumber");
+}
