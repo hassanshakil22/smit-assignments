b391edd4e332e98daea39e4a5b0b579346fdff31
hassanshakil22 <hassanshakil.workspace@gmail.com>
2024-02-03 18:20:32 +0500

    adding question 7

--- assignments/assignment_4/Ans7.dart
+++ assignments/assignment_4/Ans7.dart
@@ -1,1 +1,9 @@
-// question7
+// question7 Write a program that prints the multiplication table of a given number
+// using a for loop.
+
+void main() {
+  int number = 5;
+  for (var i = 1; i < 13; i++) {
+    print("5 x $i = ${5 * i}");
+  }
+}
