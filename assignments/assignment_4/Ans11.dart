807d7cfd59fdd5a3dae51c6b1c0a26cb4a488866
hassanshakil22 <hassanshakil.workspace@gmail.com>
2024-02-04 15:16:26 +0500

    adding question 11

--- assignments/assignment_4/Ans11.dart
+++ assignments/assignment_4/Ans11.dart
@@ -1,1 +1,14 @@
-question11
+// question11 : Write a program to display a pattern like a right angle triangle with a
+// number using loop.
+import 'dart:io';
+
+void main() {
+  int n = 10;
+
+  for (var j = 1; j < n; j++) {
+    for (var i = 1; i < j; i++) {
+      stdout.write(i);
+    }
+    print('');
+  }
+}
