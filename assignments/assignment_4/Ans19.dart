92dc790a6b1bfd90b2c97ca21f77fb662cf1d8ec
hassanshakil22 <hassanshakil.workspace@gmail.com>
2024-02-06 14:56:04 +0500

    adding question 19

--- assignments/assignment_4/Ans19.dart
+++ assignments/assignment_4/Ans19.dart
@@ -1,1 +1,20 @@
-// question19
+// question 19 : Write a program that counts the number of vowels in a given string
+// using a for loop and if-else condition.
+import 'dart:io';
+
+void main() {
+  int vowelCount = 0;
+  print("Enter a word : ");
+  String? input = stdin.readLineSync()!;
+  input.toLowerCase();
+  for (var i = 0; i < input.length; i++) {
+    if (input[i] == "a" ||
+        input[i] == "e" ||
+        input[i] == "i" ||
+        input[i] == "o" ||
+        input[i] == "u") {
+      vowelCount++;
+    }
+  }
+  print("the word $input has $vowelCount  vowels");
+}
