fc8de727235c31fabfcb8bb5c1e40cb7434ec621
hassanshakil22 <hassanshakil.workspace@gmail.com>
2024-02-06 15:39:30 +0500

    adding question 21

--- assignments/assignment_4/Ans21.dart
+++ assignments/assignment_4/Ans21.dart
@@ -1,1 +1,14 @@
-// question21
+// question 21 : Write a program that calculates the sum of the squares of all odd
+// numbers in a given list using a for loop and if-else condition.
+import 'dart:math';
+
+void main() {
+  List<int> numbers = [4, 6, 7, 4, 2, 5, 8, 9, 10, 3];
+  num sum = 0;
+  for (var number in numbers) {
+    if (number % 2 != 0) {
+      sum += pow(number, 2);
+    }
+  }
+  print(sum);
+}
