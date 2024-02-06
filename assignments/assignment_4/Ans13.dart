59774bc66dc5eea7a7271c8380de43fde809796d
hassanshakil22 <hassanshakil.workspace@gmail.com>
2024-02-06 11:50:47 +0500

    adding question 13

--- assignments/assignment_4/Ans13.dart
+++ assignments/assignment_4/Ans13.dart
@@ -1,1 +1,15 @@
-question13
+// question13 : Write a program to make such a pattern like a right angle triangle with
+// the number increased by 1 using loop..
+import 'dart:io';
+
+void main() {
+  int n = 10;
+  int number = 1;
+  for (var i = 0; i <= n; i++) {
+    for (var j = 0; j <= i; j++) {
+      stdout.write(number);
+      number++;
+    }
+    print("");
+  }
+}
